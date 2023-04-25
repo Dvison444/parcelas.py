77a2c0fb0d782bf84a78ed9c168353b3dadf11a4
Dvison444 <124817979+Dvison444@users.noreply.github.com>
2023-04-25 16:15:17 -0300

    Add files via upload

--- novoprograma de parcelas.py
+++ novoprograma de parcelas.py
@@ -7,9 +7,9 @@ print('''
 [ 2 ] duas parcelas
 [ 3 ] três parcelas                     ''')
 op = int(input('Quantas parcelas voce quer? '))
-res1 = casa / 1
-res2 = casa / 2
-res3 = casa / 3
+res1 = saldo - casa
+res2 = saldo - casa
+res3 = saldo - casa
 
 
  
@@ -24,15 +24,16 @@ if __name__ == '__main__':
 
 
 if op == 1:
-    print('A parcela selecionada ficarar no valor de {}'.format(res2))
+    print('A parcela selecionada ficarar no valor de {:.2f}'.format(res2))
 elif op == 2:
-    print('A parcela selecionada ficarar no valor de {}'.format(res2))
+    print('A parcela selecionada ficarar no valor de {:.2f}'.format(res2))
 elif op == 3:
-    print('A parcela selecionada ficarar no valor de {}'.format(res3))
+    print('A parcela selecionada ficarar no valor de {:.2f}'.format(res3))
 
 print('''
 [ 1 ] para sim
-[ 2 ] para não''')
+[ 2 ] para não
+''')
 cont = int(input('deseja continuar na compra? '))
 
 if cont == 2:
@@ -40,8 +41,16 @@ if cont == 2:
 
 if op == res1:
     print('opção selecionada')
+
+
 if saldo < casa:
-    print('compra invalida')
+    print('''
+|XXXXXXXXXXXXXXXXXXXXXXXXX|    
+|    compra invalida      |
+|XXXXXXXXXXXXXXXXXXXXXXXXX|''')
 elif saldo > casa:
-    print('compra bem sucedida')
+    print('''
+|=============================|   
+|    compra bem sucedida      |
+|=============================|   ''')
 
