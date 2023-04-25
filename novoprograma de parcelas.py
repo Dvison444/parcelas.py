f1302119977081d9a6b8957858d9991eb9ff9190
Dvison444 <124817979+Dvison444@users.noreply.github.com>
2023-04-25 16:03:20 -0300

    programa 2, adicionando mais funções

--- novoprograma de parcelas.py
+++ novoprograma de parcelas.py
@@ -1,16 +1,47 @@
+from time import sleep
+
 saldo = int(input('Quanto de saldo voce tem? '))
 casa = int(input('qual o valor da casa? '))              
 print('''
 [ 1 ] uma parcela
 [ 2 ] duas parcelas
 [ 3 ] três parcelas                     ''')
-op = int(input('Quantas parcelas voce quer?'))
-res1 = (saldo + casa) / 1
-res2 = (saldo + casa) / 2
-res3 = (saldo + casa) / 3
+op = int(input('Quantas parcelas voce quer? '))
+res1 = casa / 1
+res2 = casa / 2
+res3 = casa / 3
+
+
+ 
+if __name__ == '__main__':
+ 
+    delay = 5        # em segundos
+ 
+    print('processando…')
+    sleep(delay)
+    print('')
+
+
+
+if op == 1:
+    print('A parcela selecionada ficarar no valor de {}'.format(res2))
+elif op == 2:
+    print('A parcela selecionada ficarar no valor de {}'.format(res2))
+elif op == 3:
+    print('A parcela selecionada ficarar no valor de {}'.format(res3))
+
+print('''
+[ 1 ] para sim
+[ 2 ] para não''')
+cont = int(input('deseja continuar na compra? '))
+
+if cont == 2:
+    exit()
+
 if op == res1:
     print('opção selecionada')
 if saldo < casa:
     print('compra invalida')
 elif saldo > casa:
     print('compra bem sucedida')
+
